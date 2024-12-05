c78937f9e8e6bdfe525c4c6145f19efac0509c1f
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2024-12-05 14:39:19 -0500

    Code: test: funny non-enumeration test

--- code/Tests/Test.Non-Emumeration/Test.EntryPoint.cs
+++ code/Tests/Test.Non-Emumeration/Test.EntryPoint.cs
@@ -11,27 +11,48 @@
 namespace StateMachines {
     using Console = System.Console;
 
+    // Transition graph:
+    // NegativeInfinity <-> MinValue <-> Epsilon <-> MaxValue <-> PositiveInfinity
+    // MinValue <-> MaxValue
+    // NaN -> anything, anything -X-> NaN
 
     class Test {
 
+        static readonly double[] nonNaN = new double[] { double.NegativeInfinity, double.MinValue, double.Epsilon, double.MaxValue, double.PositiveInfinity, };
+
         static StateMachine<double> PopulateNumericStateMachine() {
             StateMachine<double> stateMachine = new(double.NaN);
+            stateMachine.AddValidStateTransitionChain(
+                (start, finish) => { Console.Write($"Moving from {start} to {finish}: " ); },
+                undirected: true,
+                double.NegativeInfinity, double.MinValue, double.Epsilon, double.MaxValue, double.PositiveInfinity);
             stateMachine.AddValidStateTransition(double.MinValue, double.MaxValue, (start, finish) => {
-                Console.WriteLine("Maximum is one step away from minumum");
+                Console.Write($"Maximum ({start}) is one step away from minumum ({finish}) :-): ");
             }, undirected: true);
-            stateMachine.AddValidStateTransition(double.NaN, double.MinValue, (start, finish) => {
-                Console.WriteLine("Who needs NaN?");
-            });
-            stateMachine.AddValidStateTransition(double.NaN, double.MaxValue, (start, finish) => {
-                Console.WriteLine("Who needs NaN? :-)");
-            });
+            foreach (double value in nonNaN)
+                stateMachine.AddValidStateTransition(double.NaN, value, (start, finish) => 
+                    Console.Write("Who needs NaN, anyway? :-) "));
+            foreach (double value in nonNaN)
+                stateMachine.AddInvalidStateTransition(value, double.NaN, (start, finish) =>
+                    "Who needs NaN? :-) ");
             return stateMachine;
         } //PopulateBox
 
         static void Main() {
             var stateMachine = PopulateNumericStateMachine();
-            stateMachine.TryTransitionTo(double.MaxValue);
-            stateMachine.TryTransitionTo(double.MinValue);
+            Console.WriteLine(stateMachine.TryTransitionTo(double.NegativeInfinity));
+            Console.WriteLine(stateMachine.TryTransitionTo(double.MinValue));
+            Console.WriteLine(stateMachine.TryTransitionTo(double.MaxValue));
+            Console.WriteLine(stateMachine.TryTransitionTo(double.PositiveInfinity));
+            Console.WriteLine(stateMachine.TryTransitionTo(double.Epsilon));
+            try {
+                Console.WriteLine();
+                double target = 42.42;
+                Console.WriteLine($"Trying to jump to {target}:");
+                stateMachine.TryTransitionTo(target);
+            } catch (System.Exception e) {
+                Console.WriteLine($"      {e.GetType().Name}: {e.Message}");
+            } //exception
         } //Main
 
     } //class Test
